ba669505cdcc0467201af3c4c2de215921949d1f
Allan <65522943+allanbfinet@users.noreply.github.com>
2026-01-13 10:31:34 -0700

    Update customer_trusted_to_curated.py

--- glue_jobs/customer_trusted_to_curated.py
+++ glue_jobs/customer_trusted_to_curated.py
@@ -2,6 +2,7 @@ import sys
 from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 from pyspark.context import SparkContext
 
 
@@ -17,29 +18,58 @@ def main():
     )
 
     sc = SparkContext.getOrCreate()
-    glue_context = GlueContext(sc)
-    spark = glue_context.spark_session
-    job = Job(glue_context)
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+    job = Job(glueContext)
     job.init(args["JOB_NAME"], args)
 
     customer_trusted = args["S3_CUSTOMER_TRUSTED"].rstrip("/") + "/"
     accelerometer_trusted = args["S3_ACCELEROMETER_TRUSTED"].rstrip("/") + "/"
     customers_curated = args["S3_CUSTOMERS_CURATED"].rstrip("/") + "/"
 
-    cust_df = spark.read.parquet(customer_trusted)
-    accel_df = spark.read.parquet(accelerometer_trusted)
+    # --- AWS S3 SOURCES ---
+    customer_trusted_dyf = glueContext.create_dynamic_frame.from_options(
+        connection_type="s3",
+        format="parquet",
+        connection_options={"paths": [customer_trusted], "recurse": True},
+        transformation_ctx="CustomerTrusted_node",
+    )
+
+    accelerometer_trusted_dyf = glueContext.create_dynamic_frame.from_options(
+        connection_type="s3",
+        format="parquet",
+        connection_options={"paths": [accelerometer_trusted], "recurse": True},
+        transformation_ctx="AccelerometerTrusted_node",
+    )
+
+    cust_df = customer_trusted_dyf.toDF()
+    accel_df = accelerometer_trusted_dyf.toDF()
 
     # Keep only customers who actually have accelerometer data
     curated_df = (
-        cust_df.join(accel_df.select("user").dropDuplicates(), cust_df["email"] == accel_df["user"], "inner")
+        cust_df.join(
+            accel_df.select("user").dropDuplicates(),
+            cust_df["email"] == accel_df["user"],
+            "inner",
+        )
         .drop(accel_df["user"])
         .dropDuplicates(["email"])
     )
 
-    curated_df.coalesce(1).write.mode("overwrite").parquet(customers_curated)
+    curated_dyf = DynamicFrame.fromDF(curated_df, glueContext, "CustomersCurated_node")
+
+    # --- AWS S3 TARGET ---
+    glueContext.write_dynamic_frame.from_options(
+        frame=curated_dyf,
+        connection_type="s3",
+        format="parquet",
+        connection_options={"path": customers_curated},
+        transformation_ctx="CustomersCuratedSink_node",
+    )
 
     job.commit()
 
 
 if __name__ == "__main__":
     main()
+
